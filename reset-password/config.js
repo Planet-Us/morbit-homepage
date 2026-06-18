45c1d51e4110486c287d2ea0e73b044f523d5542
eunkanglee <eklee808@gmail.com>
2026-06-18 11:42:36 +0900

    버스허브앱비번바꾸기config값바꿈

--- reset-password/config.js
+++ reset-password/config.js
@@ -1,4 +1,4 @@
 window.MORBIT_SUPABASE_CONFIG = {
-  url: "https://obmmegoxrudvgivmazxv.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ibW1lZ294cnVkdmdpdm1henh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1NjExNDMsImV4cCI6MjA5NDEzNzE0M30._-OYDmS8HifAoQ1tWxsf6afdRuBtBEV4mxVx7-WhYoo"
+  url: "https://puhcegfacazxteagytaw.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1aGNlZ2ZhY2F6eHRlYWd5dGF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5MTU5NzQsImV4cCI6MjA2NjQ5MTk3NH0.0cmq8K3dfJPeldtL9DqCdd7V3bgmUEt9aBsXutj8wPE"
 };
